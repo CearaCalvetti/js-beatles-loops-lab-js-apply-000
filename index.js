58c4a52d55ebf23967550538676cf34d17bfadb7
Ceara Calvetti <calvetti.ceara@gmail.com>
2019-02-05 17:53:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var finalArray = []
+  var i;
+  for (i=0; i<musicians.length; i++) {
+    finalArray[i]=musicians[i]+" plays "+instruments[i];
+  }
+  return finalArray;
+  
+}
