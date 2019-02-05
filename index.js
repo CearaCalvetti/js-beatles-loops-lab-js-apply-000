484dcb69cfac08a8a829b32ea852852a7203c9a4
Ceara Calvetti <calvetti.ceara@gmail.com>
2019-02-05 18:16:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// add solution here
-function theBeatlesPlay(musicians, instruments) {
-  var finalArray = []
-  var i;
-  for (i=0; i<musicians.length; i++) {
-    finalArray[i]=musicians[i]+" plays "+instruments[i];
+function theBeatlesPlay(musicians, instruments){
+  var finalArray = [];
+  for (var i = 0; i < musicians.length; i++); {
+    finalArray[i] = musicians[i]+" plays "+instruments[i]
   }
-  return finalArray;
+return finalArray;
+}
 
 }
